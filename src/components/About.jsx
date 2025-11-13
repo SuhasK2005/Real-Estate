e44a1a248be720361ae2db8ce3054d198512282d
SuhasK2005 <saisuhask2005@gmail.com>
2025-11-13 21:10:58 +0530

    started testimonial section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       className=" flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
       id="About"
     >
-      <h1 className="text-2xl sm-text-4xl font-bold mb-2">
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
         About{" "}
         <span className="underline underline-offset-4 decoration-1 under font-light">
           Our Brand
